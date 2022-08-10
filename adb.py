d44c7577b2d15a27f49d88ace1e41ea52c80375a
Woosuk Nam <wsuk.nam@gmail.com>
2022-08-10 23:48:22 +0900

    220810

--- adb.py
+++ adb.py
@@ -1,4 +1,5 @@
 from ppadb.client import Client as AdbClient
+import asyncio
 
 client = AdbClient(host="127.0.0.1", port=5037)
 devices = client.devices()
@@ -7,6 +8,6 @@ device = devices[0]
 class adb_tool:
     def t_netstat(self):
         r_netstat = device.shell("netstat -tul")
-        return r_netstat
+        return "사용 중인 포트를 점검합니다.\n"+r_netstat
 
 tool = adb_tool()
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PyQt5 import uic
 from PyQt5.QtCore import QCoreApplication
 import time
 from adb import *
+import asyncio
 
 form_class = uic.loadUiType("start.ui")[0]
 form_class2 = uic.loadUiType("tool.ui")[0]
@@ -29,10 +30,14 @@ class Window_2(QMainWindow, form_class2):
         self.start_btn.clicked.connect(self.Result)
         self.clear_btn.clicked.connect(self.result_text.clear)
 
-    def Result(self):
+    async def Result_text(self):
         add_text = self.result_text.append
-        add_text("비정상 포트를 점검합니다.\n")
-        add_text(tool.t_netstat()+"\n")
+        add_text(tool.t_netstat())
+        await asyncio.sleep(1.0)
+        add_text("sleep 1")
+
+    def Result(self):
+        asyncio.run(Window_2.Result_text()) # 야 비동기; 빨리 돌으라고;
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
