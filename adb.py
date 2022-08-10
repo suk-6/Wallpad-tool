461db85f5349c179c9b421d3d29eebd8a1b6e908
Woosuk Nam <wsuk.nam@gmail.com>
2022-08-10 22:17:28 +0900

    220810

--- adb.py
+++ adb.py
@@ -4,4 +4,9 @@ client = AdbClient(host="127.0.0.1", port=5037)
 devices = client.devices()
 device = devices[0]
 
-print(device.shell("netstat -tul"))
+class adb_tool:
+    def t_netstat(self):
+        r_netstat = device.shell("netstat -tul")
+        return r_netstat
+
+tool = adb_tool()
--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from PyQt5.QtCore import QCoreApplication
-from tool import Window_2
+import time
+from adb import *
 
 form_class = uic.loadUiType("start.ui")[0]
+form_class2 = uic.loadUiType("tool.ui")[0]
 
 class Window_1(QMainWindow, form_class):
     def __init__(self):
@@ -17,6 +19,20 @@ class Window_1(QMainWindow, form_class):
         self.hide()
         self.w = Window_2()
         self.w.show()
+
+class Window_2(QMainWindow, form_class2):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.setWindowTitle("Asseertive Wallpad Tool")
+        self.close_btn.clicked.connect(QCoreApplication.instance().quit)
+        self.start_btn.clicked.connect(self.Result)
+        self.clear_btn.clicked.connect(self.result_text.clear)
+
+    def Result(self):
+        add_text = self.result_text.append
+        add_text("비정상 포트를 점검합니다.\n")
+        add_text(tool.t_netstat()+"\n")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
